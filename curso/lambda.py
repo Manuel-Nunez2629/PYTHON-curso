ece15ef442f20d9e7335efefe25a3aea92bc7e29
Manuel-Nunez2629 <148600863+Manuel-Nunez2629@users.noreply.github.com>
2024-09-04 16:28:57 -0500

    Update lambda.py

--- curso/lambda.py
+++ curso/lambda.py
@@ -3,7 +3,7 @@
 # no son aptas para cuando damos mas de una expresion no funciona
 multiplicar_por_dos = lambda x:x*2
 
-#rint( multiplicar_por_dos(2))
+#print( multiplicar_por_dos(2))
 
 #usando filter con una funcion comun
 numero=[1,2,3,4,5,6,7,8,9]
